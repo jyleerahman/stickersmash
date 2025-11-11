a3a7e1af29d82b52e8808fb1f44accf8fd475eee
JY Lee <jylee.rahman@gmail.com>
2025-11-11 11:28:35 -0500

    Add buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
 import Button from '@/components/Button';
+import CircleButton from '@/components/CircleButton';
+import IconButton from '@/components/IconButton';
 import ImageViewer from '@/components/ImageViewer';
+
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import { StyleSheet, View } from "react-native";
+
 
 const PlaceholderImage = require('@/assets/images/background-image.png')
 
@@ -24,14 +28,34 @@ export default function Index() {
     } else {
       alert('You did not select any image.')
     }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
   }
+
+  const onAddSticker = () => {
+    // will implement later
+  }
+
+  const onSaveImageAsync = async () => {
+    // will implement later
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
       ) : (
         <View style={styles.footerContainer}>
           <Button label='Choose a photo' theme='primary' onPress={pickImageAsync} />
@@ -63,5 +87,14 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
-  }
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+
 })
